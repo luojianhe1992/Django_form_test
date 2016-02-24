aa0cef61033e9e5f3a749669b0142640948ca35c
luojianhe1992 <luojianhe1992@gmail.com>
2016-02-24 16:42:23 -0500

    Backup data.

--- Django_form_test/HoneyCell_django/WebApp/models.py
+++ Django_form_test/HoneyCell_django/WebApp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+from datetime import datetime
+
 # Create your models here.
 
 
@@ -17,3 +19,16 @@ class Book(models.Model):
     book_name = models.CharField(max_length=100)
     book_description = models.CharField(max_length=100)
     book_file = models.FileField(upload_to=generate_url)
+
+
+class Memo(models.Model):
+    user = models.ForeignKey(User)
+    memo_name = models.CharField(max_length=100)
+    memo_description = models.TextField(max_length=1000)
+    memo_date = models.DateTimeField(default=datetime.now)
+
+
+class FooModel(models.Model):
+    foo_name = models.CharField(max_length=100)
+    time_changed = models.DateTimeField(auto_now=True)
+    time_created = models.DateTimeField(auto_now_add=True)
